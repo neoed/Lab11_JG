670690daeb2db078a0b69c8775ad338a1a30379f
neoed <neoed@tlen.pl>
2021-01-23 15:07:05 +0100

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,14 @@
+public class Account {
+
+    private int accountNumber;
+    private int accountMoney;
+
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountMoney(int accountMoney) {
+        this.accountMoney = accountMoney;
+    }
+}
